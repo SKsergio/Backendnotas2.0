d69cb3a26f616f04d61c82e79f112a0aca799b7c
SKsergio <sergiofloress911@gmail.com>
2025-09-19 22:53:18 -0600

    make:migration to structure of Degrees

--- database/seeders/DegreeSeeder.php
+++ database/seeders/DegreeSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\catalogues\Degree;
 
 class DegreeSeeder extends Seeder
 {
@@ -12,6 +13,34 @@ class DegreeSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Degree::create([
+            'code' => 'grd01',
+            'name' => 'Pirmer Grado'
+        ]);
+
+        Degree::create([
+            'code' => 'MED',
+            'name' => 'Medicina'
+        ]);
+
+        Degree::create([
+            'code' => '4tFDD',
+            'name' => 'cUARTO gRADO'
+        ]);
+
+        Degree::create([
+            'code' => 'FRD',
+            'name' => 'Administración'
+        ]);
+
+        Degree::create([
+            'code' => 'DGDF',
+            'name' => 'Administración'
+        ]);
+
+        Degree::create([
+            'code' => '134FGD',
+            'name' => 'Administración'
+        ]);
     }
 }
